24f1eb0e440e829bb22a451fe3286c797de1be3d
heCarlo <carlosgfkp@outlook.com>
2023-09-13 11:03:08 -0300

    aula finalizada

--- produtos/views.py
+++ produtos/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pessoa
 
 def ver_produto(request):
-    nome = 'Carlos'
-    return render(request, 'ver_produto.html', {'nome': nome})
+    if request.method == "GET":
+        nome = 'Carlos'
+        return render(request, 'ver_produto.html', {'nome': nome})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+
+        pessoas = Pessoa.objects.filter(nome=nome)
+        if pessoas.exists():
+            HttpResponse("Usuário já cadastrado")
+        else:
+            HttpResponse("Usuário cadastrado")
+        return HttpResponse("ok")
+
 
 def inserir_produto(request):
     return HttpResponse('Estou no inserir')
